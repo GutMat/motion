9efbb0428183bae2b31ae6145183ebadb4675226
GutMat <GutMateusz96@interia.pl>
2019-04-15 18:01:43 +0200

    Added motion info

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -3,7 +3,23 @@ function init() {
 }
 
 function onDeviceReady() {
-	navigator.notification.beep(1);
+    navigator.notification.beep(1);
+
+    function onSuccess(acceleration) {
+        alert('Acceleration X: ' + acceleration.x + '\n' +
+              'Acceleration Y: ' + acceleration.y + '\n' +
+              'Acceleration Z: ' + acceleration.z + '\n' +
+              'Timestamp: '      + acceleration.timestamp + '\n');
+    }
+    
+    function onError() {
+        alert('onError!');
+    }
+    
+    var options = { frequency: 2000 };  // Update every 2 seconds
+    
+    var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+       
 }
 
 function checkConnection() {
